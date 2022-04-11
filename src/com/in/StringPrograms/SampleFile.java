741fb0a3879655e35d1a935e6e17361c1291fc78
sunil4034 <giddalurusunilkumar@gmail.com>
2022-04-11 12:23:48 +0530

    second commit

---
+++ src/com/in/StringPrograms/SampleFile.java
@@ -0,0 +1,19 @@
+package com.in.StringPrograms;
+
+public class SampleFile {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10, b=20;
+		 a=a+b;
+		 b=a-b;
+		 a=a-b;
+		 System.out.println(a+"<>"+b);
+
+	}
+
+}
